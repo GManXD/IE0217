372895ee12a9ef4fe10302d5f4c5881c9106556b
GManXD <oscarrojasrojas6xc@gmail.com>
2024-03-17 19:31:55 -0600

    Agregando comentarios en el archivo estructuras.cpp
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

--- Previos/Previo2/estructuras.cpp
+++ Previos/Previo2/estructuras.cpp
@@ -3,23 +3,24 @@
 
 using namespace std;
 
+// Creando la estructura Person
 struct Person {
-    char name[50];
-    int age;
-    float salary;
+    char name[50];  // Propiedad nombre
+    int age;        // Propiedad edad
+    float salary;   // Propiedad salario
 
 };
 
 
 int main() {
-    Person p1;
+    Person p1;  // Creando la persona p1
 
     cout << "Ingrese el nombre completo";
-    cin.get(p1.name, 50);
+    cin.get(p1.name, 50);   // Guardar el nombre en la propiedad name de p1
     cout << "Ingrese la edad";
-    cin >> p1.age;
+    cin >> p1.age;          // Guardar la edad en la propiedad age de p1
     cout << "Ingrese el salario: ";
-    cin >> p1.salary;
+    cin >> p1.salary;       // Guardar el salario en la propiedad salary de p1
 
     cout << "\nDesplegar informacion." << endl;
     cout << "Nombre: " << p1.name << endl;
