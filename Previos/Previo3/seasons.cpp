4031eb31a5981ec64edfc385ba9ea306d379452d
GManXD <oscarrojasrojas6xc@gmail.com>
2024-03-20 12:22:23 -0600

    Comentando el archivo seasons.cpp
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

--- Previos/Previo3/seasons.cpp
+++ Previos/Previo3/seasons.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 
-enum seasons { 
-    spring = 34,
+// Se usa un define para MIPRIMERDEFINE
+#define MIPRIMERDEFINE 995
+enum seasons {  //Enumerador
+    spring = 34,       // Posibles valores para el tipo seasons
     summer = 4,
     autum = 9,
     winter = 32
 };
 
+// Otro enumerador
+enum enumsito{
+    carro,
+    casa,
+    arbol,
+    patio
+};
+
 int main() {
     seasons s;
-    s = summer;
+    s = summer;  // Se asigna valor a la variable "s" tipo seasons
     cout << "Summer = " << s << endl;
-
+    // del define:
+    cout << "Define: " << MIPRIMERDEFINE << endl;
+    // Se imprime el valor de arbol que se encuentra en enumsito
+    cout << "arbol: " << arbol << endl;
     return 0;
 }
