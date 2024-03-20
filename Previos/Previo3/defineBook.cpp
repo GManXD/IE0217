4f5e28160a4ec1f2486d3e13200eaff63d0180ed
GManXD <oscarrojasrojas6xc@gmail.com>
2024-03-19 22:33:53 -0600

    Añadiendo ejemplos de #define
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Previos/Previo3/defineBook.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Se define BOOK_ID_MAX
+#define BOOK_ID_MAX 10
+
+int main(){
+    // Valor de BOOK_ID_MAX
+    cout << "El valor de Book ID MAX es: " << BOOK_ID_MAX;
+    return 0;
+}
