6b2469943cc3900d27518f7855695e4d38124dbb
GManXD <oscarrojasrojas6xc@gmail.com>
2024-03-20 13:54:32 -0600

    agregando ejemplo de uso de memoria
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Previos/Previo3/memory.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int var1 = 3;
+    int var2 = 24;
+    int var3 = 16;
+
+    // Imprimir la direccion de las variables
+    cout << "Direccion de la variable1: " << &var1 << endl;
+    cout << "Direccion de la variable2: " << &var2 << endl;
+    cout << "Direccion de la variable3: " << &var3 << endl;
+    // El & antes de la variable nos lleva a la direccion de memoria
+    return 0;
+}
