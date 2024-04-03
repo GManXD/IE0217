8b625a60362d1c52f8a2cb7d084c699a56f2e0d3
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-03 02:28:57 -0600

    Agregando ideas iniciales para los archivos .hppp, .cpp y main.cpp
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Tareas/Tarea1/src/ahorcado.hpp
@@ -0,0 +1,43 @@
+#ifndef AHORCADO_HPP
+#define AHORCADO_HPP
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+extern string diccionario[];  // Diccionario de palabras
+
+struct Ahorcado {
+    string palabraAdivinar;
+    string estadoActual;    // letras encontradas y no encontradas
+    int intentosMax;
+    int intentosActual;
+};
+
+void inicializar();
+    // Elige una palabra aleatoria del diccionario e inicializa la variable "estadoActual" con giones bajos
+    // Establece el numero maximo de intentos permitidos y el numero actual de intentos realizados
+
+
+
+void adivinar();
+    // Verifica si la letra adivinada por el usuario pertenece a la palabra
+    // Actualiza el estado de la palabra a adivinar en el caso de que se adivine una letra
+
+
+void verificarGane();
+    // Verifica si se ha terminado el juego, es decir, que se adivine la palabra o se alcancen la cantidad maxima de intentos
+    // Actualiza la catidan de intentos
+
+
+void menu();
+    /*
+    Mostrar menú interactivo: 
+    1. Elegir la dificultad del juego (n´umero de intentos).
+    2. Iniciar el juego.
+    3. Agregar palabras al arreglo de palabras que se escogen aleatoriamente.
+    4. Ver diccionario de palabras.
+    5. Salir del programa.
+    */
+
+#endif // AHORCADO_HPP
---
+++ Tareas/Tarea1/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ahorcado.hpp"
+using namespace std;
+
+
+int main(){
+
+
+
+
+    return 0;
+}
