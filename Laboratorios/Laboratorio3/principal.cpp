f288ed097097e301c4057e899011e4e27f1b30e7
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-11 09:09:01 -0600

    Agregando documentacion
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

--- Laboratorios/Laboratorio3/principal.cpp
+++ Laboratorios/Laboratorio3/principal.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 struct Libro{
+    // Alberga la informacion de un libro y la direccion del que sigue
     string titulo;
     string autor;
     string genero;
-    // Puntero al siguiente libro
+    // Puntero al libro que sigue
     Libro* siguiente;
 
 };
 
 enum opciones{
+    // Opciones disponibles para el menu
     AGREGAR_LIBRO = 1,
     MOSTRAR_LIBROS = 2,
     SALIR = 3,
@@ -24,7 +26,10 @@ void agregarLibro(Libro*& lista){
     // Se asigna memoria dinamica
     Libro* nuevoLibro = new Libro;
 
+    // Ingresar informacion del libro
+
     cout << "Ingrese el titulo del libro: " << endl;
+    cin.ignore();   // Vaciar buffer
     getline(cin, nuevoLibro->titulo);
 
     cout << "Ingrese el autor del libro: " << endl;
@@ -32,7 +37,10 @@ void agregarLibro(Libro*& lista){
 
     cout << "Ingrese el genero del libro: " << endl;
     getline(cin, nuevoLibro->genero);
-
+    
+    // El puntero siguiente apunta al libro anterior
+    nuevoLibro->siguiente = lista;
+    // La variable lista ahora va a apuntar a este libro nuevo
     lista = nuevoLibro;
 
     cout << "Libro agregado correctamente " << endl;
@@ -40,7 +48,8 @@ void agregarLibro(Libro*& lista){
 
 void mostrarLibros(Libro* lista){
     // Muestra la informacion de todos los libros
-    if (lista == nullptr){
+
+    if (lista == nullptr){  // Caso que no tenga libros
         cout << "La libreria no tiene libros." << endl;
         return;
     }
@@ -50,20 +59,23 @@ void mostrarLibros(Libro* lista){
         cout << "Titulo: " << lista->titulo << endl;
         cout << "Autor: " << lista->autor << endl;
         cout << "Genero: " << lista->genero << endl << endl;
-        lista = lista->siguiente;
+        lista = lista->siguiente;  // Ahora lista apunta al libro que sigue
     }
 }
 
 void liberarMemoria(Libro* &lista){
     // Libera memoria dinamica
     while (lista != nullptr){
+        // Se crea un puntero temporal para almacenar la direccion del libro a borrar
         Libro* temp = lista;
+        // Ahora lista debe apuntar al libro que sigue
         lista = lista->siguiente;
         delete temp;
     }
 }
 
 int main(){
+    // Primer Libro puntero que no apunta a nada
     Libro* listaLibros = nullptr;
     int opcion;
     do{
@@ -72,7 +84,7 @@ int main(){
         cout << "2. Mostrar todos los libros" << endl;
         cout << "3. Salir" << endl;
         cin >> opcion;
-
+        // Casos del menu
         switch (opcion){
             case AGREGAR_LIBRO:
                 agregarLibro(listaLibros);
@@ -87,8 +99,8 @@ int main(){
                 cout << "Opcion no permitida. " << endl;
                 break;
         }
-    } while (opcion != 3);
-
+    } while (opcion != SALIR);
+    // Liberar la memoria dinamica utilizada
     liberarMemoria(listaLibros);
 
     return 0;
