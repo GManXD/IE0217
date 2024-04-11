bd1ad184412c0fb71bb2c6f68e83aceacae8ef5b
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-11 09:19:31 -0600

    Agregando archivo encabezado
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Laboratorios/Laboratorio3/libro.cpp
@@ -0,0 +1,9 @@
+#include "libro.hpp"
+
+// Define los valores de las opciones
+opciones AGREGAR_LIBRO = 1;
+opciones MOSTRAR_LIBROS = 2;
+opciones SALIR = 3;
+opciones OPCIONES_MAX = 4; // Si necesitas usar OPCIONES_MAX en tu código
+
+// Implementación de cualquier otra función relacionada con las opciones, si es necesario
