4ddeb9b369882b3461a435f328134eb5ff6d5b89
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-18 23:47:27 -0600

    Agregando la funcion de insertar un contacto en la nube
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

--- Tareas/Tarea3/src/main.cpp
+++ Tareas/Tarea3/src/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h>
+#include <cstring>
+#include <list>
+#include <utility>
 
 using namespace std;
 
-class Node{
-    // Nodos para la lista enlazada
-    public:
-        string nombre;
-        int telefono;
-        Node* next;  // Puntero hacia el siguiente nodo de la lista enlazada
-};
 
 enum opciones{
     // Opciones disponibles para el menu
@@ -22,10 +18,67 @@ enum opciones{
     OPCIONES_MAX
 };
 
+class Node{
+    // Nodos para la lista enlazada
+    public:
+        string nombre;
+        int telefono;
+        Node* next;  // Puntero hacia el siguiente nodo de la lista enlazada
+    Node(string nombre, int telefono){
+        this->nombre = nombre;
+        this->telefono = telefono;
+        this->next = nullptr;
+    }
+};
+
+
+class HashTable{
+    // Estructura que almacena una lista enlazada por cada contacto, de forma que la llave corresponde al nombre del contacto
+    // Cada valor corresponde a una lista enlazada que contiene una copia de los contactos correspondientes
+    private:
+        int tamano;     // Constante del tamaño del Hash-Table
+        vector<Node*> tabla;    // Vector que contiene punteros a nodos, este corresponde a los values de la Hash table
+    
+    public:
+        // Constructor de la clase, inicializa la tabla con nullptr
+        HashTable(int tamano): tamano(tamano), tabla(tamano,nullptr) {}
+
+        int Hashing(string key){
+            // Algoritmo de hashing
+            int suma = 0;
+            for (char c : key){
+                suma += c;  // Suma el valor ASCII de todas las letras del nombre
+            }
+            return suma % tamano;   // Retorna un indice
+        }
+        
+        void insertarContacto(string nombre, int numero){
+            // Agrega un nuevo contacto a la nube
+            int indice = Hashing(nombre);
+            Node* celdaPtr= tabla[indice];  // Nodo puntero que apunta al primer nodo de la lista
+            while (celdaPtr != nullptr){
+                if (celdaPtr->nombre == nombre){  // Caso de que el contacto ya exista
+                    cout << "El contacto ya existia" << endl;
+                    break;  // No iterar más 
+                }
+                celdaPtr = celdaPtr->next;  // Seguir recorriendo
+            }
+            Node* nuevoNodo = new Node(nombre, numero);
+            if (tabla[indice] == nullptr){  // Caso que no exista el primer nodo de la lista
+                tabla[indice] = nuevoNodo;  // El nuevo contacto se convierte en el primer nodo de la lista
+            }
+            else{   // Caso de que ya existia el primer nodo
+                    nuevoNodo->next = celdaPtr;     // El nuevo nodo apunta al nodo al que apuntaba la hash table en el indice correspondiente
+                    celdaPtr->next = nuevoNodo;     // El nodo al cual apunta la hash table en ese indice ahora corresponde a el nuevo Nodo
+            }
+        }
+
+
+};
 
 
 int main(){
-
+    HashTable tablaHash = HashTable(10);   // Se debe especificar el tamaño de la tabla
     int opcion;
     do{
         cout << "Seleccione su opcion" << endl;
@@ -38,6 +91,7 @@ int main(){
         // Casos del menu
         switch (opcion){
             case AGREGAR_CONTACTO:
+                
                 // Funcion de agregar contacto
                 break;
             case ELIMINAR_CONTACTO:
@@ -58,7 +112,7 @@ int main(){
         }
     } while (opcion != SALIR);
     // Liberar la memoria dinamica utilizada
-    
+
 
     return 0;
 }
