c80a63f587c574198c20c8ec0d0bec0a8e52bb64
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-24 11:58:28 -0600

    Agregando archivo encabezado.hpp
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Laboratorios/Laboratorio5/biblioteca.hpp
@@ -0,0 +1,30 @@
+#ifndef BIBLIOTECA_HPP
+#define BIBLIOTECA_HPP
+
+#include <string>
+#include <vector>
+
+class Libro {
+public:
+    std::string titulo;
+    std::string autor;
+    int anioPublicacion;
+
+    Libro(std::string t, std::string a, int anio);
+    
+    bool operator<(const Libro& l) const;
+};
+
+class Biblioteca {
+private:
+    std::vector<Libro> libros;
+
+public:
+    void agregarLibro(const Libro& libro);
+    void mostrarLibros();
+    void ordenarLibrosPorAnio();
+    std::vector<Libro>::iterator buscarLibroPorTitulo(const std::string& titulo);
+    std::vector<Libro>::iterator end();
+};
+
+#endif // BIBLIOTECA_HPP
