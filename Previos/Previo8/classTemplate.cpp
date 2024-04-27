fb57e9dcff78147d3fc7ccdf40e95e2fc069dd46
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-27 13:42:29 -0600

    Agregando ejemplo de plantilla de clase
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Previos/Previo8/classTemplate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Plantilla de clase
+template <class T>
+class Number {
+    private:
+        // Variable de tipo T
+        T num;
+
+    public:
+        // Constructor
+        Number(T n) : num(n) {}
+
+        // Metodo que retorna una variable de tipo T
+        T getNum() {
+            return num;
+        }
+};
+
+int main() {
+    // Crear object con parametro tipo int 
+    Number<int> numberInt(7);
+
+    // Crear object con parametro tipo double
+    Number<double> numberDouble(7.7);
+
+    cout << "int Number = " << numberInt.getNum() << endl;
+    cout << "double Number = " << numberDouble.getNum() << endl;
+
+    return 0;
+}
