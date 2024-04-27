d8f50ada18d14961e3f04cadf29f6e16cbce8cb7
GManXD <oscarrojasrojas6xc@gmail.com>
2024-04-27 13:19:23 -0600

    Agregando ejemplo de plantilla
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

---
+++ Previos/Previo8/template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+T add(T num1, T num2){
+    return (num1+num2);
+}
+
+int main(){
+    int result1;
+    double result2;
+
+    //Llamar a la plantilla con parametros tipo entero
+    result1 = add<int>(2,3);
+    cout << result1 << endl;
+
+    // Llamar a la plantilla con parametros tipo double
+    result2 = add<double>(2.2, 3.3);
+    cout << result1 << endl;
+
+    return 0;
+}
