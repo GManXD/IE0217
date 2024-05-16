1feba0fb9b81c243df32eb704c5bd07af97c2398
GManXD <oscarrojasrojas6xc@gmail.com>
2024-05-16 17:05:48 -0600

    Agregando documentacion de doxygen
    Signed-off-by: GManXD <oscarrojasrojas6xc@gmail.com>

--- Tareas/Tarea5/src/validador.hpp
+++ Tareas/Tarea5/src/validador.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file validador.hpp
+ * @version 1.0
+ * @date 16/05/2024
+ * @author Oscar Rojas Rojas
+ * @title Encabezados
+ * @brief Declaracion de clases y metodos
+*/
+
 #ifndef VALIDADOR_HPP
 #define VALIDADOR_HPP
 
@@ -5,13 +14,28 @@
 #include <regex>
 #include <stdexcept>
 
+/**
+ * @brief Clase para validar correos electrónicos.
+ */
 class ValidadorEmail {
-    // Clase para la validacion del email
+    // Clase para validar emails
 public:
     std::string correo;
 
-    void solicitarCorreo();     // Solicita el correo
-    bool validarCorreo(const std::string& correo);      // Valida el correo
+    /**
+     * @brief Solicita al usuario que ingrese un correo electrónico.
+     */
+    void solicitarCorreo(); // Solicita el correo al usuario
+
+    /**
+     * @brief Valida un correo electrónico según una serie de reglas.
+     * @param correo El correo electrónico a validar.
+     * @return true si el correo es válido, false en caso contrario.
+     * @throws std::invalid_argument si el correo no cumple con las reglas de validación.
+     */
+    bool validarCorreo(const std::string& correo);  // Valida el correo ingresado
+    
 };
 
 #endif // VALIDADOR_HPP
+
